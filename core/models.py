881816430923c113104b7cfa0b8fc0f3cc751002
iii123iii <euyhegnrh@gmail.com>
2024-11-22 09:38:16 +0200

    added security changes

--- core/models.py
+++ core/models.py
@@ -4,7 +4,7 @@ from django.utils import timezone
 
 class Post(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
-    title = models.CharField(max_length=100)
-    body = models.TextField()
+    title = models.CharField(max_length=200)
+    body = models.TextField(max_length=10000)
     file = models.FileField(upload_to='posts/', null=True, blank=True)
     created_at = models.DateTimeField(default=timezone.now)
